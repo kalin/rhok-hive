7c0e3efcec89262888cc9aca9ecf79d20bd63a67
Jérémy Fache <jfache@gmail.com>
2012-12-01 14:21:41 -0800

    defined models and setup basic admin

---
+++ hive/beelogger/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from beelogger.models import Credit, Check
+
+class CreditAdmin(admin.ModelAdmin):
+    list_display = ('user', 'format_unit')
+    readonly_fields = ['datetime']
+
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ('user', 'format_check', 'datetime')
+    list_filter = ['user', 'datetime', 'check_type']
+    readonly_fields = ['datetime']
+
+admin.site.register(Credit, CreditAdmin)
+admin.site.register(Check, CheckAdmin)
