329a14e4e1c15450471ca5f94870738f85b2e6a4
Jarek Piórkowski <jarek@piorkowski.ca>
2012-12-02 01:34:46 -0800

    some more csv views, at least one with fun subtle bug

--- hive/beelogger/urls.py
+++ hive/beelogger/urls.py
@@ -11,4 +11,6 @@ urlpatterns = patterns('',
     url(r'^check-user-state/$', views.CheckUserStateView, name='check_user_state'),
     url(r'^check-user-in-out/$', views.UserCheckInOutView, name='check_user_in_out'),
     url(r'^csvdump/$', views.CSVDumpView, name='csv_dump'),
+    url(r'^csvmonthdump/$', views.CSVDumpCurrentMonth, name='csv_month_dump'),
+    url(r'^csvuserdump/$', views.CSVDumpUser, name='csv_dump_user'),
 )
--- hive/beelogger/views.py
+++ hive/beelogger/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render_to_response
 from beelogger.models import HiveUser, Check, Credit
 import csv
-from datetime import datetime, timedelta
+from datetime import datetime, date, timedelta
 from django.db.models.aggregates import Sum
 
 class TestView(TemplateView):
@@ -56,56 +56,119 @@ def UserCheckInOutView(request):
         return render_to_response('beelogger/user-checked-in-out.html', extra_context)
 
 def CSVDumpView(request):
+    response,writer = PrepareCSVResponseObject()
+
+    for user in HiveUser.objects.all():
+        results = ProcessChecks(user.check_set.all())
+
+        for result in results:
+            writer.writerow(result)
+
+    return response
+
+def CSVDumpUser(request):
+    response,writer = PrepareCSVResponseObject()
+
+    checks = Check.objects.filter(user__user = request.GET['pin'])
+
+    for result in ProcessChecks(checks):
+        writer.writerow(result)
+
+    return response
+
+def CSVDumpCurrentMonth(request):
+    response,writer = PrepareCSVResponseObject()
+
+    today = date.today()
+
+    if 'pin' in request.GET:
+        checks = GetOneMonthsData(today.year, today.month, request.GET['pin'])
+    else:
+        #checks = []
+        #for user in HiveUser.objects.all():
+        #    checks.extend(GetOneMonthsData(today.year, today.month, user.user))
+        #TODO! this returns incorrect data, datetimes are in wrong places
+        checks = GetOneMonthsData(today.year, today.month)
+
+    for result in ProcessChecks(checks):
+        writer.writerow(result)
+
+    return response
+
+def GetOneMonthsData(year, month, specificUser = False):
+    beginMonthDate = datetime(year,month,1)
+
+    if month < 12:
+        endYear = year
+        endMonth = month + 1
+    else:
+        endYear = year + 1
+        endMonth = 1
+    
+    endMonthDate = datetime(endYear,endMonth,1) - timedelta(seconds = 1) # one second before midnight
+
+    if specificUser:
+        checks = Check.objects.filter(user__user = specificUser) \
+            .filter(datetime__gte = beginMonthDate) \
+            .filter(datetime__lte = endMonthDate)
+    else:
+        checks = Check.objects.filter(datetime__gte = beginMonthDate) \
+            .filter(datetime__lte = endMonthDate)
+    
+    return checks
+
+def PrepareCSVResponseObject():
     # proper csv headers commented out for commenting
     #response = HttpResponse(mimetype='text/csv')
     #response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
     response = HttpResponse(mimetype='text/plain')
 
     writer = csv.writer(response)
-    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+    writer.writerow(['user','in_time','out_time','visit_time','total_time', \
+        'credit_change','ccs_before','cc_unit_type'])
 
-    for user in HiveUser.objects.all():
-        writer.writerow(['test',str(user)])
+    return response,writer
 
-        month_time = dict()
-        month_hours = dict()
+def ProcessChecks(checks):
+        if len(checks) < 2:
+            return []
+
+        if checks[0].datetime > checks[2].datetime:
+            checks.reverse()
+
+        result = []
         total_time = timedelta()
-
-        checks = user.check_set.all().reverse()
+        totalling_before_date = checks[len(checks)-1].datetime
 
         i = 0
         while i < len(checks):
-            if (checks[i].check_type == 'in'):
+            if (checks[i].check_type == 'ou'):
                 # only look at 'in' checkins. 'out' records will then be the next ones
 
-                in_time = checks[i].datetime
-                out_time = checks[i+1].datetime
+                in_time = checks[i+1].datetime
+                out_time = checks[i].datetime
                 visit_time = out_time - in_time
                 total_time += visit_time
 
-                """keystring = str(in_time.year) + '-' + str(in_time.month)
-                if (keystring in month_time == False):
-                    month_time[keystring] = datetime.timedelta()
-                month_time[keystring] += visit_time"""
-
-                totalling_before_date = datetime.fromtimestamp(0)
-                totalling_before_date = datetime(2012,12,1)
-
-                credit_changes = checks[i+1].credit_set.all()
+                credit_changes = checks[i].credit_set.all()
+                cc_units = 0
+                ccs_before = 0
+                cc_unit_type = ''
                 if (len(credit_changes) > 0):
                     cc = credit_changes[0] # the credit change for this visit
+                    cc_units = cc.units
+                    cc_unit_type = cc.unit_type
                     ccs_before = Credit.objects.all() \
+                        .filter(user = checks[i].user) \
                         .filter(datetime__lte = cc.datetime) \
                         .filter(datetime__gte = totalling_before_date) \
                         .filter(unit_type = cc.unit_type) \
                         .aggregate(sum = Sum('units'))['sum']
-                    if ccs_before == None:
-                        ccs_before = 0
                 
-                writer.writerow([in_time,out_time,visit_time,month_time,total_time, \
-                    cc.units,ccs_before])
+                result.append([checks[i].user,in_time,out_time,visit_time,total_time, \
+                    cc_units,ccs_before,cc_unit_type])
 
             i += 1
 
-    return response
+        return result
 
